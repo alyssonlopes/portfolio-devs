39cdb0df837a5a0cfde43035b6f6649c30e5d1f5
Alysson Lopes <alysson.lopes@arctouch.com>
2021-10-25 20:27:19 -0300

    Utilizar API para renderizar lista de Devs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,12 +6,59 @@ import DevItem from "../../components/DevList/DevItem";
 import SubHeader from "../../components/SubHeader";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      devsList: [],
+    };
+  }
+
   handleChange = (evt) => {
     const text = evt.target.value;
     console.log(text);
   };
 
+  async componentDidMount() {
+    console.log("componentDidMount");
+
+    const response = await fetch("https://randomuser.me/api/?results=10");
+    const data = await response.json();
+
+    const devsList = data.results.map((result) => {
+      return {
+        name: `${result.name.first} ${result.name.last}`,
+        photo: result.picture.thumbnail,
+        expertise: result.email,
+      };
+    });
+
+    this.setState({
+      isLoading: false,
+      devsList,
+    });
+
+    // fetch("https://randomuser.me/api/?results=5")
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   const devsList = data.results.map((result) => {
+    //     return {
+    //       name: `${result.name.first} ${result.name.last}`,
+    //       photo: result.picture.thumbnail,
+    //       expertise: result.email,
+    //     };
+    //   });
+
+    //   this.setState({
+    //     isLoading: false,
+    //     devsList,
+    //   });
+    // });
+  }
+
   render() {
+    console.log("render");
     return (
       <>
         {/* Header */}
@@ -24,29 +71,18 @@ class Home extends React.Component {
         {/* Search */}
         <Search onChange={this.handleChange} />
         {/* List */}
-
-        <DevList>
-          <DevItem
-            photo={"https://i.pravatar.cc/100?img=1"}
-            name="Ana Lopes"
-            expertise="React Developer"
-          />
-          <DevItem
-            photo={"https://i.pravatar.cc/100?img=2"}
-            name="Silvia Luiza"
-            expertise="React Developer"
-          />
-          <DevItem
-            photo={"https://i.pravatar.cc/100?img=3"}
-            name="Caio Banguela"
-            expertise="React Developer"
-          />
-          <DevItem
-            photo={"https://i.pravatar.cc/100?img=4"}
-            name="Harry Potter"
-            expertise="React Developer"
-          />
-        </DevList>
+        {this.state.isLoading && "Loading.."}
+        {!this.state.isLoading && (
+          <DevList>
+            {this.state.devsList.map((dev) => (
+              <DevItem
+                name={dev.name}
+                photo={dev.photo}
+                expertise={dev.expertise}
+              />
+            ))}
+          </DevList>
+        )}
       </>
     );
   }
