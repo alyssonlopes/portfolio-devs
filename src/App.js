f8712ee1642a469b68054f0fb7e94abeba1e16c0
Alysson Lopes <alysson.lopes@arctouch.com>
2021-10-28 10:25:40 -0300

    Mover Rotas do App

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import React from "react";
-import Home from "./pages/Home";
-import Perfil from "./pages/Perfil";
-import Register from "./pages/Register";
+import Router from "./routes";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHomePage: true,
-      isPerfilPage: false,
-      isRegisterPage: false,
-      selectedDev: null,
-    };
-  }
-
-  goToHome = () => {
-    this.setState({
-      isHomePage: true,
-      isPerfilPage: false,
-      isRegisterPage: false,
-    });
-  };
-
-  goToPerfil = (selectedDev) => {
-    this.setState({
-      isHomePage: false,
-      isPerfilPage: true,
-      isRegisterPage: false,
-      selectedDev,
-    });
-  };
-
-  goToRegister = () => {
-    this.setState({
-      isHomePage: false,
-      isPerfilPage: false,
-      isRegisterPage: true,
-    });
-  };
-
   render() {
-    return (
-      <>
-        {this.state.isHomePage && (
-          <Home
-            onChangePagePerfil={this.goToPerfil}
-            onChangePageRegister={this.goToRegister}
-          />
-        )}
-        {this.state.isPerfilPage && (
-          <Perfil dev={this.state.selectedDev} onChangePage={this.goToHome} />
-        )}
-        {this.state.isRegisterPage && <Register onChangePage={this.goToHome} />}
-      </>
-    );
+    return <Router />;
   }
 }
 
