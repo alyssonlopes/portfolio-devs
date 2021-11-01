28454f5429b6a222c5f021904f543694569dc8d6
Alysson Lopes <alysson.lopes@arctouch.com>
2021-11-01 15:25:07 -0300

    corrigir uso de props no componente DevList

--- src/components/DevList/index.js
+++ src/components/DevList/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const DevList = () => {
-  return <ul>{this.props.children}</ul>;
+const DevList = ({ children }) => {
+  return <ul>{children}</ul>;
 };
 
 export default DevList;
